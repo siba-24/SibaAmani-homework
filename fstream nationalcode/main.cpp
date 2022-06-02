8d21d327fd6663dea218a580c9fda5dc94fadb62
siba-24 <99756241+siba-24@users.noreply.github.com>
2022-06-02 17:10:26 +0430

    Add files via upload

---
+++ fstream nationalcode/main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+float mark;
+string name;
+string nationalcode;
+fstream m;
+void inputdata();
+void printdata();
+int main()
+{
+    inputdata();
+    printdata();
+    return 0;
+}
+void inputdata()
+{
+    m.open("sample.txt",ios::out);
+    for (int i = 1;i > name;
+    cout << nationalcode;
+    cout << mark;
+    m > name)
+    {
+        bank >> nationalcode;
+        bank >> mark;
+        if (mark >=15)
+        {
+            find = 1;
+            cout << endl;
+            cout << "name is :" << name << "\t" << "codemeli is : " << nationalcode << "\t" << "moadel is :" << mark <<endl;
+        }
+    }
+    if ('"f"' = 0)
+    {
+        cout << "any one can not ger mark great than 15" << endl;
+    }
+}
+    
